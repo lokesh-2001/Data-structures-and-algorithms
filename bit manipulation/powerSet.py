1a3652ac2d47174976f9187ef4f77e6e57faec51
Lokesh Dhingra <lokesh2001dhingra@gmail.com>
2022-01-07 19:12:24 +0530

    Delete bit manipulation directory

--- bit manipulation/powerSet.py
+++
@@ -1,7 +0,0 @@
-from itertools import combinations
-def print(str):
-    for i in range(0,len(str)+1):
-        for el in combinations(str,i):
-            print(''.join(el))
-str=['a','b','c']
-print(str) 
